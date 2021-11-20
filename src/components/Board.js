b3464ecf31dbd066a9d5863750b7b1f29923bbbd
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2021-11-21 00:01:06 +0600

    file added

---
+++ src/components/Board.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Square from "./Square";
+import calculateWinner from "../helper/functions";
+
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  // handleclick
+  handleClick = (i) => {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      // if state true return false and vice versa
+      xIsNext: !this.state.xIsNext,
+    });
+  };
+
+  // method to get data
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    // const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+
+    // winner selection
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="wrapper">
+        <div className="width">
+          <h2 className="status">{status}</h2>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
